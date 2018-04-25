ec41ca111dd964bafce863f111dd2a1f4d3e4e12
ade bakari <redariez@gmail.com>
2018-04-24 20:00:07 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+//Create a add COMMENT
+function getinfo(){
+  var username = document.getElementById("username").value
+  var comment = document.getElementById("comment").value
+
+
+  var userNameText = document.createTextNode("Username: " + username)
+  var commentText = document.createTextNode("Comment: " + comment)
+
+
+  var img = document.createElement("img")
+  var newListItem = document.createElement("Li")
+  newListItem.className = "group-list-item"
+  var newUserName = document.createElement("H3")
+  newUserName.appendChild(userNameText)
+  var newComment = document.createElement("P")
+  newComment.appendChild(commentText)
+
+
+
+
+  newListItem.appendChild(newUserName)
+  newListItem.appendChild(newComment)
+  document.getElementById("commentList").appendChild(newListItem)
+
+}
